9c26546cf9d01aefd56aa2cf5413b5d92208ffaa
q10357 <isch004@student.kristiania.no>
2022-04-06 13:24:57 +0200

    npm start for heroku support

---
+++ client/index.jsx
@@ -0,0 +1,4 @@
+import react from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+//Parcel puts its output in the dist directory
+//We use this o serve static files
+app.use(express.static("../client/dist"))
+
 const server = app.listen( process.env.PORT || 3000, () => {
     console.log(`Started on http://localhost:${server.address().port}`);
 });
